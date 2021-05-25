f762edbd2167aecfa768329cd0c2bac8ef5d377d
uvelyster <48861250+uvelyster@users.noreply.github.com>
2021-05-25 18:37:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('good job. it works \n');
+  res.send('nice job. it works. HaeKwon \n');
 });
 
 app.listen(PORT);
